b3c78dd298378f53c610255c20b9f47fc75d0357
Lav1n1a <lavinia.camk01@gmail.com>
2026-03-09 18:55:09 -0400

    fix: Ajuste para github Pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: `/rocket-blog`
+   plugins: [react()],
+  base: `/rocket-blog/`
 })
